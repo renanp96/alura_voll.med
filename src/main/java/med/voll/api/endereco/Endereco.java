9f10bca3c4508b1c917a548131f59a7abd301485
renanp96 <renanpa1996@gmail.com>
2023-09-21 11:14:34 -0300

    feature: criadas JPAs e Persistencias das classes de entrada e saida de valores

--- src/main/java/med/voll/api/endereco/Endereco.java
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -1,3 +1,20 @@
 package med.voll.api.endereco;
 
-public record Endereco(String logradouro, String bairro, String cep, String cidade, String uf, String complemento, String numero) { }
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String cidade;
+    private String uf;
+    private String complemento;
+    private String numero;
+}
