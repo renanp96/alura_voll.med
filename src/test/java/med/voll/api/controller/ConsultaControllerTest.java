e0291f684bc08b0ec1dbf21eb4390702cb222396
renanp96 <renanpa1996@gmail.com>
2023-11-08 11:04:43 -0300

    feature: criados teste unitarios do repositories e controllers

---
+++ src/test/java/med/voll/api/controller/ConsultaControllerTest.java
@@ -0,0 +1,77 @@
+package med.voll.api.controller;
+
+import med.voll.api.domain.consulta.AgendaDeConsultas;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.consulta.DadosDetalhamentoConsulta;
+import med.voll.api.domain.medico.Especialidade;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.json.AutoConfigureJsonTesters;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@AutoConfigureJsonTesters
+class ConsultaControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private JacksonTester<DadosAgendamentoConsulta> dadosAgendamentoConsultaJson;
+
+    @Autowired
+    private JacksonTester<DadosDetalhamentoConsulta> dadosDetalhamentoConsultaJson;
+
+    @MockBean
+    private AgendaDeConsultas agendaDeConsultas;
+
+    @Test
+    @DisplayName("Retornar erro 400 ao receber dados invalidos.")
+    @WithMockUser
+    void agendar_cenario1() throws Exception {
+        MockHttpServletResponse response = mvc.perform(post("/consultas")).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    @WithMockUser
+    @DisplayName("Retorna codigo 200 quando os dados estiverem validas")
+    void agendar_cenario2() throws Exception {
+        LocalDateTime data = LocalDateTime.now().plusHours(1);
+        Especialidade especialidade = Especialidade.CARDIOLOGIA;
+        var dadosDetalhamento = new DadosDetalhamentoConsulta(null, 2L, 5L, data);
+
+        when(agendaDeConsultas.agendar(any())).thenReturn(dadosDetalhamento);
+
+        MockHttpServletResponse response = mvc.perform(
+                post("/consultas").contentType(MediaType.APPLICATION_JSON).content(dadosAgendamentoConsultaJson.write(
+                        new DadosAgendamentoConsulta(2L, 5l, data, especialidade)
+                ).getJson())
+                ).andReturn().getResponse();
+
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+
+        String jsonResponse = dadosDetalhamentoConsultaJson.write(
+                new DadosDetalhamentoConsulta(null, 2L, 5L, data)
+        ).getJson();
+
+        assertThat(response.getContentAsString()).isEqualTo(jsonResponse);
+    }
+}
